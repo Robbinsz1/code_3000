f17f3db1b04de4179269b0cadfddfdce5801d549
= <=>
2026-09-18 19:44:51 -0400

    bruh

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -13,8 +13,8 @@ def train_model(X, y, seed=seed):
         learning_rate=0.1,
         n_estimators=100,
         max_depth=4,
-        subsample=1,
-        min_samples_leaf=1,
+        subsample=2,
+        min_samples_leaf=2,
         random_state=seed
     )
     model.fit(X, y)
